2c8523c0c60a9ee67281405c1546f410eb257950
Adirelle <adirelle+github@gmail.com>
2018-02-07 08:45:52 +0100

    Added context.Context related funcs to logging.

---
+++ logging/context.go
@@ -0,0 +1,42 @@
+package logging
+
+import (
+	"context"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"go.uber.org/zap"
+)
+
+type contextKey int
+
+var loggerKey = contextKey(1)
+
+func ContextWithLogger(ctx context.Context, l Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, l)
+}
+
+func FromContext(ctx context.Context) Logger {
+	maybeLogger := ctx.Value(loggerKey)
+	if logger, ok := maybeLogger.(Logger); ok {
+		return logger
+	}
+	return Wrap(zap.L(), nil)
+}
+
+func RequestWithLogger(req *http.Request, l Logger) *http.Request {
+	return req.WithContext(ContextWithLogger(
+		req.Context(),
+		l.With(
+			"uniqueId", strconv.FormatUint(rand.Uint64(), 16),
+			"method", req.Method,
+			"url", req.URL,
+			"remote", req.RemoteAddr,
+		),
+	))
+}
+
+func FromRequest(req *http.Request) Logger {
+	return FromContext(req.Context())
+}
